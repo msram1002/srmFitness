beecf60545789235d3c6b1794f50e4c02640e636
Sundara Ram Matta <msram0210@gmail.com>
2021-09-21 21:55:24 -0400

    adding animations using gsap

--- app.js
+++ app.js
@@ -11,3 +11,66 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-hero', {
+  duration: 1,
+  opacity: 0,
+  y: -200,
+  stagger: 0.5,
+  delay: 0.2
+})
+
+gsap.from('.animate-services', {
+  scrollTrigger: '.animate-services',
+  duration: 0.5,
+  opacity: 1,
+  x: -150,
+  stagger: 0.2
+})
+
+gsap.from('.animate-img', {
+  scrollTrigger: '.animate-img',
+  duration: 2,
+  opacity: 0,
+  x: 500,
+  stagger: 0.5
+})
+
+gsap.from('.animate-membership', {
+  scrollTrigger: '.animate-membership',
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.2,
+  delay: 0.5
+})
+
+gsap.from('.animate-team', {
+  scrollTrigger: '.animate-team',
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.5,
+  delay: 0.4
+})
+
+gsap.from('.animate-email', {
+  scrollTrigger: '.animate-email',
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.5,
+  delay: 0.4
+})
+
+gsap.from('.animate-footer', {
+  scrollTrigger: '.animate-footer',
+  duration: 1,
+  opacity: 0,
+  y: 100,
+  stagger: 0.4,
+  delay: 0.2
+})
